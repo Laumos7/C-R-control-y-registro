ad29ab3c7e77e155be597991a1f9cf1da9ddb45d
Laumos7 <31476638+Laumos7@users.noreply.github.com>
2017-12-03 14:20:36 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>BIENVENIDOS</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="view/main.css" />
+		<link rel="shortcut icon" href="view/favicon.ico">
+	</head>
+	
+	<body class="loading">
+		<div id="wrapper">
+			<div id="bg">
+		</div>
+		
+		<div id="overlay">
+			
+		</div>
+
+		<div id="main">
+
+				<!-- Header -->
+					<header id="header">
+						<h1 href="#forgot">BIENVENIDOS</h1>
+						<p>Un Sistema De Registro Para Un Control Oportuno</p>
+						<nav>
+							<ul>
+								<li><a href="#" class="icon fa-car"><span class="label">Twitter</span></a></li>
+								<li><a href="#" class="icon fa-commenting"><span class="label">Facebook</span></a></li>
+								<li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+								<li><a href="#" class="icon fa-bicycle"><span class="label">Github</span></a></li>
+								<li><a href="#" class="icon fa-hand-peace-o"><span class="label">Email</span></a></li>
+							</ul>
+
+								<h1><a href="view/LOGIN.php">Ingresar</a></h1>
+						</nav>
+
+
+					</header>
+
+			</div>
+		</div>
+
+		<script>
+			window.onload = function() { document.body.className = ''; }
+			window.ontouchmove = function() { return false; }
+			window.onorientationchange = function() { document.body.scrollTop = 0; }
+		</script>
+	</body>
+</html>
